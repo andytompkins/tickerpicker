5c6ad3992dea2ce00ba9a2ec692d61eb93255011
andytompkins <andy.tompkins@gmail.com>
2015-12-01 10:44:53 -0800

    added some scripts to index

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+
+$('.typeahead-symbols').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+}, {
+  name: 'symbols',
+  source: 
+})
